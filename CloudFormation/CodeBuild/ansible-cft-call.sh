dfc2f12e593426db42d11b33f87548399bb5293b
rajeshreddy.rapolu@gmail.com <rajeshreddy.rapolu@gmail.com>
2018-07-21 07:41:27 +0530

    Updated

--- CloudFormation/CodeBuild/ansible-cft-call.sh
+++ CloudFormation/CodeBuild/ansible-cft-call.sh
@@ -11,7 +11,7 @@ env | grep -i AWS_
 echo "running ansible playbook $ANSIBLE_PLAYBOOK_PATH $ANSIBLE_PLAYBOOK_TAGS"
 
 if [ -z "$ANSIBLE_PLAYBOOK_TAGS" ]; then
-	ansible-playbook -i CloudFormation/CodeBuild/hosts "$ANSIBLE_PLAYBOOK_PATH"
+	ansible-playbook localhost -i CloudFormation/CodeBuild/hosts "$ANSIBLE_PLAYBOOK_PATH"
 else
-	ansible-playbook -i CloudFormation/CodeBuild/hosts "$ANSIBLE_PLAYBOOK_PATH" -t "$ANSIBLE_PLAYBOOK_TAGS"
+	ansible-playbook localhost -i CloudFormation/CodeBuild/hosts "$ANSIBLE_PLAYBOOK_PATH" -t "$ANSIBLE_PLAYBOOK_TAGS"
 fi
