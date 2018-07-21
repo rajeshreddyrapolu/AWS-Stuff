50c96a2b5784d048496b3bf1c71c4bc78b646ed5
rajeshreddy.rapolu@gmail.com <rajeshreddy.rapolu@gmail.com>
2018-07-21 07:26:57 +0530

    Updated

--- CloudFormation/CodeBuild/ansible-cft-call.sh
+++ CloudFormation/CodeBuild/ansible-cft-call.sh
@@ -2,10 +2,8 @@
 
 unset AWS_SESSION_TOKEN
 
-temp_role=$(aws sts assume-role \
-					--role-arn "arn:aws:iam::097878324419:role/admins" \
-					--role-session-name "CloudFormationSession")
-echo "Testing"
+temp_role=$(aws sts assume-role --role-arn "arn:aws:iam::097878324419:role/admins" --role-session-name "CloudFormationSession")
+echo "temp_role $temp_role"
 export AWS_ACCESS_KEY_ID=$(echo $temp_role | jq .Crredentials.AccessKeyId | xargs)
 export AWS_SECRET_ACCESS_KEY=$(echo $temp_role | jq .Crredentials.SecretAccessKey | xargs)
 export AWS_ACCESS_KEY_ID=$(echo $temp_role | jq .Crredentials.AccessKeyId | xargs)
